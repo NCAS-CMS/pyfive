df0f8ecabc188aaa7e6f080dc3d3911fb423984b
zequihg50 <ezequiel.cimadevilla@unican.es>
2025-10-02 11:58:15 +0200

    adopted new test suite

--- tests/test_compact.py
+++ tests/test_compact.py
@@ -1,19 +1,36 @@
 import os
 
 import numpy as np
+import pytest
 from numpy.testing import assert_array_equal
 
 import pyfive
-
-DIRNAME = os.path.dirname(__file__)
-DATASET_COMPACT_HDF5_FILE = os.path.join(DIRNAME, 'compact.hdf5')
+import h5py
 
 
-def test_compact_dataset():
-
-    with pyfive.File(DATASET_COMPACT_HDF5_FILE) as hfile:
-        data = np.array([1, 2, 3, 4], dtype=np.int32)
-
+def test_compact_dataset_hdf5(name, data):
+    with pyfive.File(name) as hfile:
         # check data
         dset1 = hfile['compact']
         assert_array_equal(dset1[...], data)
+
+
+@pytest.fixture(scope='module')
+def data():
+    return np.array([1, 2, 3, 4], dtype=np.int32)
+
+
+@pytest.fixture(scope='module')
+def name(data):
+    name = os.path.join(os.path.dirname(__file__), 'compact.hdf5')
+
+    f = h5py.File(name, 'w', libver='earliest')
+    dtype = h5py.h5t.NATIVE_INT32
+    space = h5py.h5s.create_simple(data.shape)
+    dcpl = h5py.h5p.create(h5py.h5p.DATASET_CREATE)
+    dcpl.set_layout(h5py.h5d.COMPACT)
+    dset_id = h5py.h5d.create(f.id, b"compact", dtype, space, dcpl=dcpl)
+    dset_id.write(h5py.h5s.ALL, h5py.h5s.ALL, data)
+    f.close()
+
+    return name
