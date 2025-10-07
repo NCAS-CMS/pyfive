7cb0531adc7c2f18446f41fcc9358b1a25033a8f
Bryan Lawrence <bryan.lawrence@ncas.ac.uk>
2025-10-07 09:37:11 +0100

    Cleaning up

--- pyfive/datatype_msg.py
+++ pyfive/datatype_msg.py
@@ -169,15 +169,6 @@ class DatatypeMessage(object):
         # and padded to an 8 byte boundary, the number of which is given by the 
         # message size.
         size =  datatype_msg['size']
-        #if nbufs:
-        #    tag_size = nbufs * 8
-        #    tag_bytes = self.buf[self.offset:self.offset+tag_size]
-        #    null_location = tag_bytes.find(b'\x00')
-        #    if null_location != -1:
-        #        tag = tag_bytes[:null_location].decode('ascii')
-        #    else:
-        #        tag = tag_bytes.decode('ascii')
-        #    self.offset += tag_size
         null_location = self.buf.index(b'\x00', self.offset)
         tag_size = _padded_size(null_location - self.offset + 1, 8)
         tag_bytes = self.buf[self.offset:self.offset+tag_size]
