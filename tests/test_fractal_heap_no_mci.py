f4adb191f838de1cf104fbb0ad7023874a3d7068
zequihg50 <ezequiel.cimadevilla@unican.es>
2026-05-01 11:44:32 +0200

    forgot pre-commit

--- tests/test_fractal_heap_no_mci.py
+++ tests/test_fractal_heap_no_mci.py
@@ -2,9 +2,11 @@ from pathlib import Path
 import pytest
 import pyfive
 
+
 @pytest.fixture(scope="module")
 def test_file_path():
     return Path(__file__).parent / "data" / "fractal_heap_no_mci_rlat.nc"
+
 
 # reported in https://github.com/NCAS-CMS/pyfive/issues/225
 def test_consolidated_metadata(test_file_path):
